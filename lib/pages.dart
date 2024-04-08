f5217268c64bc71bd4fdea7f0507dcd6c35a9e90
Maxon2024 <maxon2023kg@gmail.com>
2024-04-08 16:29:49 +0600

    fff

--- lib/pages.dart
+++ lib/pages.dart
@@ -20,21 +20,61 @@ class _HomeState extends State<Home> {
           ),
         ),
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 100,
+              backgroundColor: Colors.black,
+              backgroundImage: AssetImage('assets/images/photo1696045534.jpeg'),
+            ),
+            Text(
+              'Бурканов Максат',
+              style: TextStyle(fontSize: 40, color: Colors.black),
+            ),
+            Text(
+              'maxon2023kg@gmail.com',
+              style: TextStyle(color: Colors.black, fontSize: 20),
+            )
+          ],
+        ),
+      ),
       bottomNavigationBar: Container(
         width: double.infinity,
         height: 60,
         color: Colors.white,
-        child: Row(
+        child: const Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(Icons.badge_rounded),
-            Icon(Icons.map),
-            Icon(Icons.favorite_sharp),
-            Icon(Icons.portrait_rounded),
-            Text('Лента'),
-            Text('Карта'),
-            Text('Избранные'),
-            Text('Профиль'),
+            Column(
+              children: [
+                Icon(Icons.badge_rounded),
+                
+                Text('Лента'),
+              ],
+            ),
+            SizedBox(width: 30),
+            Column(
+              children: [
+                Icon(Icons.map),
+                Text('Карта'),
+              ],
+            ),
+            SizedBox(width: 20),
+            Column(
+              children: [
+                Icon(Icons.favorite_sharp),
+                Text('Избранные'),
+              ],
+            ),
+            SizedBox(width: 20),
+            Column(
+              children: [
+                Icon(Icons.portrait_rounded),
+                Text('Профиль'),
+              ],
+            ),
           ],
         ),
       ),
